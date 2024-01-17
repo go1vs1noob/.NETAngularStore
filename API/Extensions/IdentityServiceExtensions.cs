8023fac5dcafd04cfd6aa985769ee71ba77cb4ed
Dmitriy Krivochenko <dima.krivochenko@mail.ru>
2024-01-17 14:14:35 +0300

    Add token settings inside identity service

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,8 +1,11 @@
 
+using System.Text;
 using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
@@ -10,7 +13,7 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services,
          IConfiguration configuration)
-        {   
+        {
             services.AddDbContext<AppIdentityDbContext>(opt =>
             {
                 opt.UseSqlite(configuration.GetConnectionString("IdentityConnection"));
@@ -21,7 +24,18 @@ namespace API.Extensions
             })
             .AddEntityFrameworkStores<AppIdentityDbContext>()     // store users inside database
             .AddSignInManager<SignInManager<AppUser>>();
-            services.AddAuthentication();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // sending bearer token to be recieved by identity
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true, // make sure it's signed by our server
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:Key"])), // which key to check against (the same from TokenService)
+                        ValidIssuer = configuration["Token:Issuer"], // make sure issuer is our server
+                        ValidateIssuer = true
+                    };
+                });
             services.AddAuthorization();
 
             return services;
